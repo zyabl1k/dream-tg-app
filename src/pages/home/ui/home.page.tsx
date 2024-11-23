ec22d0a9053f5cbac71503f03c40bf5ce599a7dc
zyabl1k <kdaniild@inbox.ru>
2024-11-23 19:11:34 +0500

    feat: fix

--- src/pages/home/ui/home.page.tsx
+++ src/pages/home/ui/home.page.tsx
@@ -69,6 +69,7 @@ export const HomePage = () => {
         )}
       >
         <Header />
+        {user?.id}
         <motion.p
           key="paragraph"
           className="my-4 text-center text-muted"
