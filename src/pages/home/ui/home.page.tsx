ae8df68c76ebabe6cba13849110715a9a96d56ab
zyabl1k <kdaniild@inbox.ru>
2024-11-30 16:58:50 +0500

    feat: some fixes with slider personnel

--- src/pages/home/ui/home.page.tsx
+++ src/pages/home/ui/home.page.tsx
@@ -53,7 +53,7 @@ export const HomePage = () => {
   const blocksY = useTransform(scrollYProgress, [0.03, 0.1], [-20, 0])
 
   useEffect(() => {
-    window.scrollTo(0, -1)
+    window.scrollTo(0, 10000)
     stepsStore.set(0)
     if (BackButton) {
       BackButton.hide()
